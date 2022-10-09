5ba037c5b770461e196fcf97c4c285fbed611687
Michal Wiraszka <michal@wiraszka.com>
2022-10-09 00:12:37 -0400

    feat(frontend): Create basic app layout; test(frontend): Write a couple basic tests for footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BetEditorComponent } from './bet-editor/bet-editor.component';
 import { BetTableComponent } from './bet-table/bet-table.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { BetEditorComponent } from './bet-editor/bet-editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BetEditorComponent,
     BetTableComponent,
     FooterComponent,
     HeaderComponent,
-    BetEditorComponent,
   ],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
